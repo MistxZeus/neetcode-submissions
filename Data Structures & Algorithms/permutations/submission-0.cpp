3f065a0cf527f546f459c2c9d925ae7ed980e6f8
MistxZeus <kumar.arnav.tomar@gmail.com>
2026-09-02 19:43:13 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        return helper(0,nums);
+    }
+    vector<vector<int>>helper(int i,vector<int>&nums){
+        if(i==nums.size()){
+            return {{}};
+        }
+        vector<vector<int>>resPerms;
+        vector<vector<int>>perms=helper(i+1,nums);
+        for(vector<int>p:perms){
+            for(int j=0;j<p.size()+1;j++){
+                vector<int>pCopy(p);
+                pCopy.insert(pCopy.begin()+j,nums[i]);
+                resPerms.push_back(pCopy);
+            }
+        }
+        return resPerms;
+
+    }
+};
